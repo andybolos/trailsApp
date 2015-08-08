d48ca2dce269f521e34986a44ff8cfa4de4caaf2
Andy Bolos <andybolos@gmail.com>
2015-08-07 22:13:29 -0600

    weather

--- app/services/weatherService.js
+++ app/services/weatherService.js
@@ -4,9 +4,9 @@ app.service('weatherService', function($http, $q) {
         var dfd = $q.defer();
         $http({
             method: 'GET',
-            url: 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + '&cnt=3'
+            url: 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + '&units=imperial&cnt=3'
         }).then(function(response) {
-            dfd.resolve(response)
+            dfd.resolve(response.data)
         }), function(error) {
             console.log('Error', error);
         }
